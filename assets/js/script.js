6825c3672fe6c582d923b3a0cc510ab1eeb01193
Diego Cardenas <diego.cardenast@gmail.com>
2023-10-09 21:02:43 +0000

    feat: Add event listeners to update game screens 1 and 2.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,38 @@
-//Wait the DOM to finish loading before running the game
-//Get the button elements and add event listeners to them
-
+//Loads the second screen in the DOM to select the number of questions to play
 document.getElementsByClassName("start-button")[0].addEventListener("click",function(){
 
     document.getElementsByClassName("game-area1")[0].style.display = "none";
     document.getElementsByClassName("game-area2")[0].style.display = "flex";
     console.log("Screen1 changes to screen2");
-    
+
 });
 
-//document.getElementById("screen1").style.display = none;
+//Checks the question amount. Takes the question amount and hides the second screen
+document.getElementsByClassName("go-button")[0].addEventListener("click",function(){
+
+    let numQuestions;
+    
+    numQuestions = parseInt(document.getElementById("quests").value);
+    
+    if(numQuestions===5){
+        console.log(numQuestions);
+        document.getElementsByClassName("game-area2")[0].style.display = "none";
+        console.log("screen2 changes to first question");
+    }else if(numQuestions===10){
+        console.log(numQuestions);
+        document.getElementsByClassName("game-area2")[0].style.display = "none";
+        console.log("screen2 changes to first question");
+    }else if(numQuestions===15){
+        console.log(numQuestions);
+        document.getElementsByClassName("game-area2")[0].style.display = "none";
+        console.log("screen2 changes to first question");
+    }else if(numQuestions===20){
+        console.log(numQuestions);
+        document.getElementsByClassName("game-area2")[0].style.display = "none";
+        console.log("screen2 changes to first question");
+    }else{
+        alert(`Please select a valid number`)
+    }
+
+    
+});
