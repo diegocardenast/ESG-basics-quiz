36d7532b13e4e4631a7941947fa4d3a4e466f043
Diego Cardenas <diego.cardenast@gmail.com>
2023-10-11 12:50:48 +0000

    feat: Add showQuestions function to show first question in the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //Loads the second screen in the DOM to select the number of questions to play
-document.getElementsByClassName("start-button")[0].addEventListener("click",function(){
+document.getElementsByClassName("start-button")[0].addEventListener("click", function () {
 
     document.getElementsByClassName("game-area1")[0].style.display = "none";
     document.getElementsByClassName("game-area2")[0].style.display = "flex";
@@ -8,44 +8,55 @@ document.getElementsByClassName("start-button")[0].addEventListener("click",func
 });
 
 //Checks the question amount. Takes the question amount and hides the second screen
-document.getElementsByClassName("go-button")[0].addEventListener("click",function(){
+document.getElementsByClassName("go-button")[0].addEventListener("click", function () {
 
     let numQuestions;
-    
+
     numQuestions = parseInt(document.getElementById("quests").value);
-    
-    if(numQuestions===5){
+
+    if (numQuestions === 5) {
         console.log(numQuestions);
         document.getElementsByClassName("game-area2")[0].style.display = "none";
         console.log("screen2 changes to first question");
         assignQuestions(numQuestions);
-    }else if(numQuestions===10){
+    } else if (numQuestions === 10) {
         console.log(numQuestions);
         document.getElementsByClassName("game-area2")[0].style.display = "none";
         console.log("screen2 changes to first question");
         assignQuestions(numQuestions);
-    }else if(numQuestions===15){
+    } else if (numQuestions === 15) {
         console.log(numQuestions);
         document.getElementsByClassName("game-area2")[0].style.display = "none";
         console.log("screen2 changes to first question");
         assignQuestions(numQuestions);
-    }else if(numQuestions===20){
+    } else if (numQuestions === 20) {
         console.log(numQuestions);
         document.getElementsByClassName("game-area2")[0].style.display = "none";
-        console.log("screen2 changes to first question");
+        console.log("screen2 changes to question screen");
         assignQuestions(numQuestions);
-    }else{
+    } else {
         alert(`Please select a valid number`)
     }
 
-    
+
 });
+
+//Declaring a global array with the assigned questions for the game
+const assignedQuestions = new Array();
+//Declaring a number that tells in which question the user is
+let currentPosition = 0;
+// Declaring answer buttons
+let buttons;
+//Declaring score variable
+let score = 100;
+//Declaring check answer control
+let checkControl = 0;
 
 /**
  * It takes the amount of questions selected by user 
- * and randomly assign them from an Array of questions
+ * and randomly assign them from an Array of questions.
  */
-function assignQuestions(operand1){
+function assignQuestions(operand1) {
     //Array of objects holding all the trivia questions
     const questions = [
         //1
@@ -232,37 +243,180 @@ function assignQuestions(operand1){
 
     let randomNum;
     const assignedNums = new Array();
-    const assignedQuestions = new Array();
     let x;
     console.log(operand1); //control log
 
-    //It provides random numbers that can be used to select the questions from the questions array
-    for(let i = 0; i < operand1;i++){
-        
-        do{
-            randomNum = Math.floor(Math.random() * 20) + 1;
-        }while(assignedNums.includes(randomNum));
-        
+    //It provides random numbers that can be used to select the questions from the questions array and do not reapeat
+    for (let i = 0; i < operand1; i++) {
+
+        do {
+            randomNum = Math.floor(Math.random() * 20);
+        } while (assignedNums.includes(randomNum));
+
         assignedNums.push(randomNum);
     };
 
     console.log(assignedNums); //control log
 
     //It assigns the selected questions into an array called assignedQuestions
-    for(let y=0; y < assignedNums.length;y++){
+    for (let y = 0; y < assignedNums.length; y++) {
         x = assignedNums[y];
         assignedQuestions.push(questions[x]);
-        
     }
 
     console.log(assignedQuestions); //control log
-
+    //It shows the question screen
+    document.getElementsByClassName("game-questions-area")[0].style.display = "flex";
+    showQuestions(0);
 };
 
 /**
- * It takes the assignedQuestions array and starts showing it in the DOM 
+ * It takes a number to insert it in the array of assignedQuestions and starts showing it in the DOM 
  */
-function showQuestions(operand1){
-    
+function showQuestions(num1) {
+
+    console.log(num1); //control log
+
+    let questionScreen = document.getElementsByClassName("game-questions-area");
+
+    console.log(assignedQuestions); //control log
+
+    if (num1 < assignedQuestions.length) {
+        questionScreen[0].innerHTML = `
+            <div class="question-area">
+                <h1 class="question-number">Question ${currentPosition+1} of ${assignedQuestions.length}</h1>
+                <p>${assignedQuestions[currentPosition].questionText}</p>
+            </div>
+            <div class="answer-area">
+                <span class="answer-option">${assignedQuestions[currentPosition].option1}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option2}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option3}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option4}</span>
+            </div>
+            <div class="control-area">
+                <span class="next-question">Next</span>
+            </div>`;
+
+        console.log("question printed. Our current question is " + currentPosition + 1); //control log
+
+    } else {
+        questionScreen.innerHTML = `
+            <div class="question-area">
+                <h1 class="question-number">Question ${assignedQuestions.length} of ${assignedQuestions.length}</h1>
+                <p>${assignedQuestions[currentPosition].questionText}</p>
+            </div>
+            <div class="answer-area">
+                <span class="answer-option">${assignedQuestions[currentPosition].option1}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option2}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option3}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option4}</span>
+            </div>
+            <div class="control-area">
+                <span class="next-question">Submit</span>
+            </div>`;
+    };
+
+    document.getElementsByClassName("score-bar")[0].style.display = "flex";
+    buttons = document.getElementsByClassName("answer-option");
+    checkAnswer();
 
 }
+
+/**
+ * It assign event listeners to buttons and checks the answer from the user with the correct one saved in the array 
+ */
+function checkAnswer() {
+    checkControl = 1;
+    console.log(buttons); //control log
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            console.log(this.innerText);
+            console.log(assignedQuestions[currentPosition].correctAnswer);
+            if (this.value === assignedQuestions[currentPosition].correctAnswer) {
+                this.style.backgroundColor = "#6A971B";
+            } else {
+                score = score - (100 / assignedQuestions.length);
+                this.style.backgroundColor = "#B1106B";
+                for (i = 0; i < 4; i++) {
+                    if (document.getElementsByClassName("answer-option")[i].innerText === assignedQuestions[currentPosition].correctAnswer) {
+                        document.getElementsByClassName("answer-option")[i].style.backgroundColor = "#6A971B";
+                    }
+                }
+            };
+
+            currentPosition++;
+            console.log(`The correct answer is: ${assignedQuestions[currentPosition].correctAnswer}`); //control log
+            nextQuestion();
+        })
+    }
+    
+}
+
+
+function nextQuestion() {
+
+    let questionScreen = document.getElementsByClassName("game-questions-area");
+    //Score updates on screen
+    document.getElementsByClassName("score-bar")[0].innerText = `Score: ${score}%`;
+
+    if (currentPosition < assignedQuestions.length) {
+        questionScreen[0].innerHTML = `
+            <div class="question-area">
+                <h1 class="question-number">Question ${currentPosition+1} of ${assignedQuestions.length}</h1>
+                <p>${assignedQuestions[currentPosition].questionText}</p>
+            </div>
+            <div class="answer-area">
+                <span class="answer-option">${assignedQuestions[currentPosition].option1}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option2}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option3}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option4}</span>
+            </div>
+            <div class="control-area">
+                <span class="next-question">Next</span>
+            </div>`;
+
+        console.log("question printed. Our current question is " + currentPosition + 1); //control log
+        checkControl = 0;
+
+        document.getElementsByClassName("next-question")[0].addEventListener("click",function(){
+            if(checkControl===0){
+                checkAnswer();
+            }
+                nextQuestion();
+        });
+
+    } else if(currentPosition+1===assignedQuestions.length) {
+        questionScreen.innerHTML = `
+            <div class="question-area">
+                <h1 class="question-number">Question ${assignedQuestions.length} of ${assignedQuestions.length}</h1>
+                <p>${assignedQuestions[currentPosition].questionText}</p>
+            </div>
+            <div class="answer-area">
+                <span class="answer-option">${assignedQuestions[currentPosition].option1}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option2}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option3}</span>
+                <span class="answer-option">${assignedQuestions[currentPosition].option4}</span>
+            </div>
+            <div class="control-area">
+                <span class="submit">Submit</span>
+            </div>`;
+        
+        checkControl = 0;
+
+        document.getElementsByClassName("submit")[0].addEventListener("click",function(){
+            if(checkControl===0){
+                checkAnswer();
+                finishGame();
+            }else{
+                finishGame();
+            }
+
+        });
+
+    };
+}
+
+function finishGame() {
+    console.log(`Game finished. Score: ${score}%`);
+}
+
